7932a7a65625a7bc428e94cf0df294401acd92f6
dinglindong <lindong.ding@pactera.com>
2021-10-30 15:47:38 +0800

    feat: 添加useMount and useDebounce hooks

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import qs from 'qs'
 import { SearchPanel } from './search-panel'
 import { List } from './list'
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,24 +12,26 @@ export const ProjectListScreen = () => {
 		name: '',
 		personId: ''
 	})
+	const debouncedParam = useDebounce(param, 2000)
+	
 	const [users, setUsers] = useState([])
 	const [list, setList] = useState([])
 	
 	useEffect(() => {
-		fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
+		fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async res => {
 			if (res.ok) {
 				setList(await res.json())
 			}
 		})
-	}, [param])
+	}, [debouncedParam])
 	
-	useEffect(() => {
+	useMount(() => {
 		fetch(`${apiUrl}/users`).then(async res => {
 			if(res.ok) {
 				setUsers(await res.json())
 			}
 		})
-	}, [])
+	})
 	
 	return <div>
 		<SearchPanel users={users} param={param} setParam={setParam} />
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 export const isFalsy = (value) => value === 0 ? false : !!value
 
 // 在一个函数里， 改变传入的对象本身是不好的
@@ -11,4 +12,21 @@ export const cleanObject = (obj) => {
 		}
 	})
 	return result
+}
+
+export const useMount = (callback) => {
+	useEffect(() => {
+		callback()
+	}, []) // eslint-disable-line react-hooks/exhaustive-deps
+}
+
+export const useDebounce = (value, delay) => {
+	const [debouncedValue, setDebouncedValue] = useState(value)
+	
+	useEffect(() => {
+		const timeout = setTimeout(() => setDebouncedValue(value), delay)
+		return () => clearTimeout(timeout)
+	}, [value, delay])
+	
+	return debouncedValue
 }
